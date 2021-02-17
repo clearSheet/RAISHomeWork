7e5f1769081e30087e8e147be16d82c81e2bbf47
clearSheet <clearSheat@gmail.com>
2021-02-17 11:07:21 +0300

    first commit

---
+++ src/Test.java
@@ -0,0 +1,218 @@
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+import java.util.ArrayList;
+
+public class Test {
+    private static final int N = 2; // Hare
+    private static final int M = 5; // Tortoise
+    private static ArrayList<Racer> list;
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.createRacers(N, M);
+        Graphics graphics = new Graphics() {
+            @Override
+            public Graphics create() {
+                return null;
+            }
+
+            @Override
+            public void translate(int x, int y) {
+
+            }
+
+            @Override
+            public Color getColor() {
+                return null;
+            }
+
+            @Override
+            public void setColor(Color c) {
+
+            }
+
+            @Override
+            public void setPaintMode() {
+
+            }
+
+            @Override
+            public void setXORMode(Color c1) {
+
+            }
+
+            @Override
+            public Font getFont() {
+                return null;
+            }
+
+            @Override
+            public void setFont(Font font) {
+
+            }
+
+            @Override
+            public FontMetrics getFontMetrics(Font f) {
+                return null;
+            }
+
+            @Override
+            public Rectangle getClipBounds() {
+                return null;
+            }
+
+            @Override
+            public void clipRect(int x, int y, int width, int height) {
+
+            }
+
+            @Override
+            public void setClip(int x, int y, int width, int height) {
+
+            }
+
+            @Override
+            public Shape getClip() {
+                return null;
+            }
+
+            @Override
+            public void setClip(Shape clip) {
+
+            }
+
+            @Override
+            public void copyArea(int x, int y, int width, int height, int dx, int dy) {
+
+            }
+
+            @Override
+            public void drawLine(int x1, int y1, int x2, int y2) {
+
+            }
+
+            @Override
+            public void fillRect(int x, int y, int width, int height) {
+
+            }
+
+            @Override
+            public void clearRect(int x, int y, int width, int height) {
+
+            }
+
+            @Override
+            public void drawRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
+
+            }
+
+            @Override
+            public void fillRoundRect(int x, int y, int width, int height, int arcWidth, int arcHeight) {
+
+            }
+
+            @Override
+            public void drawOval(int x, int y, int width, int height) {
+
+            }
+
+            @Override
+            public void fillOval(int x, int y, int width, int height) {
+
+            }
+
+            @Override
+            public void drawArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+
+            }
+
+            @Override
+            public void fillArc(int x, int y, int width, int height, int startAngle, int arcAngle) {
+
+            }
+
+            @Override
+            public void drawPolyline(int[] xPoints, int[] yPoints, int nPoints) {
+
+            }
+
+            @Override
+            public void drawPolygon(int[] xPoints, int[] yPoints, int nPoints) {
+
+            }
+
+            @Override
+            public void fillPolygon(int[] xPoints, int[] yPoints, int nPoints) {
+
+            }
+
+            @Override
+            public void drawString(String str, int x, int y) {
+
+            }
+
+            @Override
+            public void drawString(AttributedCharacterIterator iterator, int x, int y) {
+
+            }
+
+            @Override
+            public boolean drawImage(Image img, int x, int y, ImageObserver observer) {
+                return false;
+            }
+
+            @Override
+            public boolean drawImage(Image img, int x, int y, int width, int height, ImageObserver observer) {
+                return false;
+            }
+
+            @Override
+            public boolean drawImage(Image img, int x, int y, Color bgcolor, ImageObserver observer) {
+                return false;
+            }
+
+            @Override
+            public boolean drawImage(Image img, int x, int y, int width, int height, Color bgcolor, ImageObserver observer) {
+                return false;
+            }
+
+            @Override
+            public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, ImageObserver observer) {
+                return false;
+            }
+
+            @Override
+            public boolean drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, Color bgcolor, ImageObserver observer) {
+                return false;
+            }
+
+            @Override
+            public void dispose() {
+
+            }
+        }
+    }
+
+    // p. a)
+    private void createRacers(int N, int M) {
+        for (int i = 0; i < N; i++) {
+            list.add(new Hare("Hare", 0, 0));
+        }
+        for (int j = 0; j < M; j++) {
+            list.add(new Tortoise("Tortoise", 0, 0));
+        }
+
+        for (int k = 0; k < 5; k++) {
+            for (Racer racer : list) {
+                racer.move();
+                racer.draw();
+                System.out.println(racer.getX());
+                System.out.println(racer.getY());
+            }
+        }
+
+    }
+
+
+}
